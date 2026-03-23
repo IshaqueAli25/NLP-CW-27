b37bac04b79c87d5c55467baf4b831ec73a97a17
Mir Ishaque Ali <129744912+IshaqueAli25@users.noreply.github.com>
2026-03-23 20:49:52 +0000

    Update Task_1.1.py

---
+++ Task_1.1.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Sentiment overall
+df['Sentiment'].value_counts().plot(kind='bar')
+plt.title("Overall Sentiment Distribution")
+plt.show()
+
+# Sarcasm overall
+df['Sarcasm'].value_counts().plot(kind='bar')
+plt.title("Overall Sarcasm Distribution")
+plt.show()
+
+# Sentiment by variety
+sns.countplot(x='variety', hue='Sentiment', data=df)
+plt.title("Sentiment across varieties")
+plt.show()
+
+# Sarcasm by variety
+sns.countplot(x='variety', hue='Sarcasm', data=df)
+plt.title("Sarcasm across varieties")
+plt.show()
